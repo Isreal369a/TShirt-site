21e28c3dbe0bd3c5fa9ea539188c55728a6675cd
isrealade <isreal369a@gmail.com>
2022-04-19 18:57:06 +0100

    UI

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,19 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
-function MyForm() {
-  return (
-    <form>
-      <label>Enter your name:
-        <input type="text" />
-      </label>
-      <label>Enter your name:
-        <input type="text" />
-      </label>
+import { useState } from "react";
+function App() {
+  const [url, setUrl] = useState("");
+  const [keyword, setKeyword] = useState("");
+  const [message, setMessage] = useState("");
+
+
+let handleSubmit = async (e) => {
+  e.preventDefault();
+  try {
+    let response = await fetch("https://localhost:5001/SearchEngineScrapper/GetResult?Url=infotrack.co.uk&Keyword=land%20registry%20search" , {
+      method: "GET",
+   
+    });
+    let resJson = await response.json();
+    if (response.status === 200) {
+      setUrl("");
+      setKeyword("");
+      setMessage(resJson);
+    } else {
+      setMessage(resJson);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+return (
+  <div className="App">
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={url}
+        placeholder="url"
+        onChange={(e) => setUrl(e.target.value)}
+      />
+      <input
+        type="text"
+        value={keyword}
+        placeholder="keyword"
+        onChange={(e) => setKeyword(e.target.value)}
+      />
       
+      <button type="submit">Create</button>
+
+      <div className="message">{message ? <p>{message}</p> : null}</div>
     </form>
-  )
+  </div>
+);
+
+
+
+
+
 }
-
-
-
-export default MyForm;
+export default App;
