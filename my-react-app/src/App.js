f09a5fb44150e8833551789031370e1e2b094275
isrealade <isreal369a@gmail.com>
2022-04-19 17:05:46 +0100

    created form

---
+++ my-react-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+function MyForm() {
+  return (
+    <form>
+      <label>Enter your name:
+        <input type="text" />
+      </label>
+      <label>Enter your name:
+        <input type="text" />
+      </label>
+      
+    </form>
+  )
+}
+
+
+
+export default MyForm;
